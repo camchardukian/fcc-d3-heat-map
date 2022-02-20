e65b537c434167b85cf65f5269ccb91a05b36851
Cameron Chardukian <camchardukian@gmail.com>
2022-02-20 14:13:28 -0600

    feature/title-and-description-and-fetch_data

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log('script is loaded into index.html')
 console.log('confirm D3 methods are available', d3)
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const response = await fetch(
+        "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
+    );
+    if (!response.ok) {
+        const errorMessage = `An error has occured: ${response.status}`;
+        throw new Error(errorMessage);
+    }
+    const data = await response.json();
+    const { baseTemperature, monthlyVariance } = data;
+    console.log("baseTemperature", baseTemperature);
+    console.log("monthlyVariance", monthlyVariance);
+
+});
